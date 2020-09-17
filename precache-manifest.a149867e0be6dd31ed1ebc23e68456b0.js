ea7110a836afa383420f6649b366d8c1131e7460
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-09-17 01:44:18 -0300

    Updates

---
+++ precache-manifest.a149867e0be6dd31ed1ebc23e68456b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc3729ec06c7d2c0afe5c0a7c0c6b05",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f8a5865cd8a8da06fced",
+    "url": "./static/css/2.3807f13a.chunk.css"
+  },
+  {
+    "revision": "130a53aab0ebee0ed31b",
+    "url": "./static/css/main.7a258136.chunk.css"
+  },
+  {
+    "revision": "f8a5865cd8a8da06fced",
+    "url": "./static/js/2.8733a663.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.8733a663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "130a53aab0ebee0ed31b",
+    "url": "./static/js/main.26e5a957.chunk.js"
+  },
+  {
+    "revision": "65c712cda5e00060133d",
+    "url": "./static/js/runtime-main.5305928b.js"
+  },
+  {
+    "revision": "5ec4a17099250ea8c37af382b01f5295",
+    "url": "./static/media/v-logo.5ec4a170.png"
+  }
+]);
